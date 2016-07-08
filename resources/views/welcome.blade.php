3d02d0d24d09240788cf0df16b9b30f980bd0abc
dansrocks <drocks2000@gmail.com>
2016-07-08 22:22:07 +0200

    Change look for welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,22 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
+				<div class="panel-heading">Welcome to Email-Marketing Management</div>
  
 				<div class="panel-body">
-					You are using bootstrap
+					<p>Are you looking for easy and user-friendly tool to manage your
+					bulletins, newsletter, masive customer notifications...?</p>
+					<p>You are lucky.  EMM is all you need. Do you want to know how EMM
+						could help you? <a href="#pending">Let's go to take a quickly
+						tour</a></p>
+				</div>
+			</div>
+
+			<div class="panel panel-default">
+				<div class="panel-heading">Quick access:</div>
+
+				<div class="panel-body">
+					<p><a class="btn btn-success" href="{{ route('campaigns.list') }}">Manage Campaigns</a></p>
 				</div>
 			</div>
 		</div>
